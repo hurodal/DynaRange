39083e8ef059140f75a4754bdc72ff80ed75f204
Juanma Font <juanmafont@gmail.com>
2025-09-08 20:47:33 +0200

    Removed orphan folder core/gui

--- src/core/gui/EventIDs.hpp
+++
@@ -1,6 +0,0 @@
-// core/gui/EventIDs.hpp
-#pragma once
-
-// 'constexpr' define una constante en tiempo de compilación.
-// Es seguro incluir este fichero en múltiples sitios sin causar errores de enlazado.
-constexpr int ID_START_BUTTON_FROM_TAB = 2001;
