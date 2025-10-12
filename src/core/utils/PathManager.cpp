75d123cbf2dc9c8bcd8b355808739e71273926d8
Juanma Font <juanmafont@gmail.com>
2025-10-12 21:03:57 +0200

    macOS fixed compilation

--- src/core/utils/PathManager.cpp
+++ src/core/utils/PathManager.cpp
@@ -15,6 +15,11 @@
 #include <unistd.h> // For getuid
 #endif
 
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#include <limits.h>
+#endif
+
 namespace { // Anonymous namespace for internal helpers
 
 fs::path GetExecutablePath() {
@@ -22,7 +27,14 @@ fs::path GetExecutablePath() {
     WCHAR path[MAX_PATH] = {0};
     GetModuleFileNameW(NULL, path, MAX_PATH);
     return fs::path(path);
-#else
+#elif __APPLE__
+    char path[PATH_MAX];
+    uint32_t size = sizeof(path);
+    if (_NSGetExecutablePath(path, &size) == 0) {
+        return fs::path(path);
+    }
+    return ""; // Return empty on error
+#else // Linux
     char result[PATH_MAX];
     ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
     return std::string(result, (count > 0) ? count : 0);
